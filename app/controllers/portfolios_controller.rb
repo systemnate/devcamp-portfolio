7fac3d21ffa1400ca4f81535da9759cd3e4946dc
Nate Dalo <ndalo@merchantsbonding.com>
2017-03-14 21:15:35 -0500

    implemented edit/update portfolio functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,6 +19,22 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Record updated successfully' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   private
 
   def portfolio_params
