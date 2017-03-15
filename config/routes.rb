75d5447453a4c464358a92f5c7d36d336542c24a
Nate Dalo <ndalo@merchantsbonding.com>
2017-03-15 06:38:02 -0500

    added link to toggle blog status

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   resources :portfolios
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   get 'about', to: 'pages#home'
   get 'contact', to: 'pages#contact'
