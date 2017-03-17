4823062331bb30edd87c719ce1ed74035fc5fad8
Nate Dalo <ndalo@merchantsbonding.com>
2017-03-17 06:21:45 -0500

    adds topic model and associates with blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,8 @@
 class Blog < ApplicationRecord
+  belongs_to :topic
+  validates :title, :body, presence: true
+
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
-
-  validates :title, :body, presence: true
 end
