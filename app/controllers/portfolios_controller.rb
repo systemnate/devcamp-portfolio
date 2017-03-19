c3e292306a3bced54d260b4218d5560fe13670c1
Nate Dalo <ndalo@merchantsbonding.com>
2017-03-19 18:51:23 -0500

    implemented for configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,6 +9,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
@@ -52,6 +53,7 @@ class PortfoliosController < ApplicationController
   private
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, 
+                                      :thumb_image, technologies_attributes: [:name])
   end
 end
