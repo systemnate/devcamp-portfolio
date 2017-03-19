918e5a2707b7ce0968fb8351ded31a639045cbf0
Nate Dalo <ndalo@merchantsbonding.com>
2017-03-19 08:58:13 -0500

    adds defaults for images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,12 @@
 class Portfolio < ApplicationRecord
   validates :title, :body, :main_image, :thumb_image, presence: true
-
   scope :rails, -> { where(subtitle: "Ruby on Rails") }
+  after_initialize :set_defaults
+
+  private
+
+  def set_defaults
+    self.main_image  ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
